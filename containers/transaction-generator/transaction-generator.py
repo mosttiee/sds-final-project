066c3eb90ce06626c8439d3d55e7bab736881211
Thamonwan Tangchitdee <thamonwan_most@hotmail.com>
2019-12-04 10:58:55 +0700

    Update transaction generator.

--- containers/transaction-generator/transaction-generator.py
+++ containers/transaction-generator/transaction-generator.py
@@ -22,13 +22,17 @@ print "sleeping for 25 to wait for webserver"
 time.sleep(25)
 
 while True:
+	try:
+		transactionAmount = round(random.uniform(1000, 100000),2)
+		interestRate = .04232 #random.uniform(.04634)
 
-	transactionAmount = round(random.uniform(1000, 100000),2)
-	interestRate = .04232 #random.uniform(.04634)
-
-	print "Computing interest for transaction with amount: " + str(transactionAmount) + " and interestRate: " + str(interestRate)
-	transaction = {'amount': str(transactionAmount), 'interestRate': str(interestRate)}
-	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-	response = requests.post("http://compute-interest-api:8080/computeinterest", data=json.dumps(transaction), headers=headers)	
-	print response.text
-	time.sleep(1)
+		print "Computing interest for transaction with amount: " + str(transactionAmount) + " and interestRate: " + str(interestRate)
+		transaction = {'amount': str(transactionAmount), 'interestRate': str(interestRate)}
+		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+		response = requests.post("http://compute-interest-api:8080/computeinterest", data=json.dumps(transaction), headers=headers)	
+		print response.text
+		time.sleep(1)
+	except:
+		print "Cannot connect to api."
+		time.sleep(2)
+	
